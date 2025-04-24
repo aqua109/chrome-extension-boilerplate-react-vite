4ca6b9b01c414c73ee7a172726145d186ef1a762
Aidan Quayle <aqua109@aucklanduni.ac.nz>
2025-04-24 14:03:17 +1200

    Revert "Testing google chart integration"
    This reverts commit 3a9c836edd71e243a1e2677ab22b7b2e70d1c898.

--- chrome-extension/src/background/index.tsx
+++ chrome-extension/src/background/index.tsx
@@ -4,7 +4,7 @@ import loadTrackerDB from '@ghostery/trackerdb';
 // var pageRequests: { [url: string]: number } = {};
 var waitForPageRequests: boolean = false;
 
-chrome.runtime.onMessage.addListener(async (request, sender, sendResponse) => {
+chrome.runtime.onMessage.addListener((request, sender, sendResponse) => {
   switch (request.type) {
     case 'queryGemini':
       switch (request.func) {
@@ -27,8 +27,7 @@ chrome.runtime.onMessage.addListener(async (request, sender, sendResponse) => {
       break;
 
     case 'initiatePageScanner':
-      scanTrackers();
-      // initialisePageRequestLogging();
+      initialisePageRequestLogging();
       break;
 
     case 'displayPageScanResults':
@@ -39,8 +38,6 @@ chrome.runtime.onMessage.addListener(async (request, sender, sendResponse) => {
           });
         }
       });
-      await sleep(100);
-      scanTrackers();
       break;
   }
 });
@@ -60,7 +57,6 @@ chrome.commands.onCommand.addListener(function (command) {
 
     // Default Ctrl+Shift+3
     case 'scan':
-      scanTrackers();
       // initialisePageRequestLogging();
       break;
   }
@@ -214,52 +210,45 @@ const pageRequestsListener = (
   }
 };
 
-const scanTrackers = async (/*pageRequests: { [url: string]: number }*/) => {
+const scanTrackers = async (pageRequests: { [url: string]: number }) => {
   let matches: object[] = [];
 
-  chrome.tabs.query({ active: true, currentWindow: true }, function (tab) {
-    if (tab[0] != null) {
-      chrome.tabs.sendMessage(tab[0].id!, {
-        type: 'scanRequestsReturned',
-      });
-    }
-  });
-  // const path = chrome.runtime.getURL('/trackerdb.engine');
-  // fetch(path)
-  //   .then(response => response.blob())
-  //   .then(blob => {
-  //     let reader = new FileReader();
-  //     reader.onload = async function (e) {
-  //       var bytes = new Uint8Array(reader.result as ArrayBuffer);
-  //       let trackerDB = await loadTrackerDB(bytes);
+  const path = chrome.runtime.getURL('/trackerdb.engine');
+  fetch(path)
+    .then(response => response.blob())
+    .then(blob => {
+      let reader = new FileReader();
+      reader.onload = async function (e) {
+        var bytes = new Uint8Array(reader.result as ArrayBuffer);
+        let trackerDB = await loadTrackerDB(bytes);
 
-  //       for (let key in pageRequests) {
-  //         const urlMatches = trackerDB.matchUrl(
-  //           {
-  //             url: key,
-  //             type: 'xhr',
-  //           },
-  //           {
-  //             getDomainMetadata: true,
-  //           },
-  //         );
+        for (let key in pageRequests) {
+          const urlMatches = trackerDB.matchUrl(
+            {
+              url: key,
+              type: 'xhr',
+            },
+            {
+              getDomainMetadata: true,
+            },
+          );
 
-  //         if (Object.keys(urlMatches).length > 0) {
-  //           matches.push(urlMatches);
-  //         }
-  //       }
+          if (Object.keys(urlMatches).length > 0) {
+            matches.push(urlMatches);
+          }
+        }
 
-  //       chrome.tabs.query({ active: true, currentWindow: true }, function (tab) {
-  //         if (tab[0] != null) {
-  //           chrome.tabs.sendMessage(tab[0].id!, {
-  //             type: 'scanRequestsReturned',
-  //             data: JSON.stringify(matches),
-  //           });
-  //         }
-  //       });
-  //     };
-  //     reader.readAsArrayBuffer(blob);
-  //   });
+        chrome.tabs.query({ active: true, currentWindow: true }, function (tab) {
+          if (tab[0] != null) {
+            chrome.tabs.sendMessage(tab[0].id!, {
+              type: 'scanRequestsReturned',
+              data: JSON.stringify(matches),
+            });
+          }
+        });
+      };
+      reader.readAsArrayBuffer(blob);
+    });
 };
 
 const enableDivHighlighting = async (func: string) => {
@@ -290,10 +279,10 @@ const initialisePageRequestLogging = async () => {
       var listener = (e: chrome.webRequest.WebRequestBodyDetails) => pageRequestsListener(e, pageRequests);
       chrome.webRequest.onBeforeRequest.addListener(listener, { urls: [] }, []);
       waitForPageRequests = true;
-      await sleep(1000);
+      await sleep(10000);
       chrome.webRequest.onBeforeRequest.removeListener(listener);
       waitForPageRequests = false;
-      // scanTrackers(pageRequests);
+      scanTrackers(pageRequests);
     }
   });
 };
--- pages/content/src/index.tsx
+++ pages/content/src/index.tsx
@@ -1,6 +1,5 @@
 import { createRoot } from 'react-dom/client';
 import Modal from './modal';
-import PieGraph from './piegraph';
 import injectedStyle from '@src/modal.css?inline';
 import { GhosteryMatch, TrackingReponse } from './ghostery-tracking-response';
 import { run } from 'node:test';
@@ -11,7 +10,7 @@ const runOnStart = async () => {
   await chrome.runtime.sendMessage({ type: 'initiatePageScanner' });
 };
 
-// runOnStart();
+runOnStart();
 
 const enableClickToAISummary = async (func: string) => {
   switch (func) {
@@ -139,31 +138,23 @@ chrome.runtime.onMessage.addListener((message, sender, sendResponse) => {
       var loader = document.getElementById('summary-loader');
       loader?.remove();
       modalTitle!.textContent = 'Site Requests Analytics';
-      // let responses: TrackingReponse = JSON.parse(message.data);
-      // let categories: { [category: string]: Array<GhosteryMatch> } = {};
-      // console.log(responses);
-      // responses.forEach(element => {
-      //   if (element.length > 0) {
-      //     element.forEach(match => {
-      //       if (categories[match.category.key] === undefined) {
-      //         categories[match.category.key] = [];
-      //       }
-      //       categories[match.category.key].push(match);
-      //     });
-      //   }
-      // });
+      let responses: TrackingReponse = JSON.parse(message.data);
+      let categories: { [category: string]: Array<GhosteryMatch> } = {};
+      console.log(responses);
+      responses.forEach(element => {
+        if (element.length > 0) {
+          element.forEach(match => {
+            if (categories[match.category.key] === undefined) {
+              categories[match.category.key] = [];
+            }
+            categories[match.category.key].push(match);
+          });
+        }
+      });
 
-      // for (let key in categories) {
-      //   formatSectionTextAndContent(toTitleCase(key), categories[key].length.toString());
-      // }
-      var src = document.createElement('script');
-      src.setAttribute('src', 'https://www.gstatic.com/charts/loader.js');
-      var modalContent = document.getElementById('ai-modal-content');
-      var pieGraph = document.createElement('div');
-      modalContent!.appendChild(src);
-      modalContent!.appendChild(pieGraph);
-      createRoot(pieGraph).render(<PieGraph />);
-
+      for (let key in categories) {
+        formatSectionTextAndContent(toTitleCase(key), categories[key].length.toString());
+      }
       break;
 
     case 'displayScanResults':
